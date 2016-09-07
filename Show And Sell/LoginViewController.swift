b773a7b170c45d299af8cc84a4dc7b0e1279d3eb
mcjcloud <brayden14cloud@gmail.com>
2016-09-07 10:21:52 -0500

    Created View Controllers

---
+++ Show And Sell/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Show And Sell
+//
+//  Created by Brayden Cloud on 9/5/16.
+//  Copyright © 2016 Brayden Cloud. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("view did load")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
